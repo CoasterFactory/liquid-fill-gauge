9fa1401a004b0c54a21d7774321af3be0c65f13d
Vincent Wilkie <vwilkie@coasterfactory.com>
2017-10-25 14:17:29 -0400

    Debugging

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var d3 = require('d3');
 
+console.log(d3);
+
 /*!
  * @license Open source under BSD 2-clause (http://choosealicense.com/licenses/bsd-2-clause/)
  * Copyright (c) 2015, Curtis Bratton
